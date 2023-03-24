be3c922d8799b9d8e6ed73d900c6b0257e5fe924
Rogger Oliveira <118573331+Roggersushi@users.noreply.github.com>
2023-03-23 21:08:19 -0300

    Add files via upload

--- Caixa Eletrônico/conta.py
+++ Caixa Eletrônico/conta.py
@@ -1,24 +1,30 @@
+from historico import Historico
 
 class Conta:
-    def __init__(self, numero, cliente, saldo, limite,):
+
+    def __init__(self, numero, cliente, saldo, limite):
         self.numero = numero
-        self.titurlar = cliente
+        self.titular = cliente
         self.saldo = saldo
-        self.limte = limite
+        self.limite = limite
+        self.historico = Historico()
+
 
     def exibir(self):
           print(f'Número da Conta:{self.numero}\n'
-                f'Nome do Titular:{self.titurlar}\n'
+                f'Nome do Titular:{self.titular}\n'
                 f'Saldo da Conta:{self.saldo}\n'
-                f'Limite da Conta:{self.limte}')
+                f'Limite da Conta:{self.limite}')
 
     def deposita(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append('depósito de {} '.format(valor))
 
     def saca(self, valor):
         if self.saldo >= valor:
             self.saldo -= valor
             print(f'O seu novo  saldo é $:{self.saldo}')
+            self.historico.transacoes.append('saque de {} '.format(valor))
 
         else:
             print(f'Vc não tem DINHEIRO!!!!')
@@ -26,10 +32,13 @@ class Conta:
     def extrato(self):
         print(f'O saldo da conta de número {self.numero} é R$: {self.saldo}')
 
-    def transfere(self,destino,valor):
+
+    def transfere_para(self,destino,valor):
         if self.saldo >= valor:
            self.saldo -= valor
            destino.saldo += valor
+           self.historico.transacoes.append('transferência de {} para conta {}'.format(valor , destino.numero))
+           destino.historico.transacoes.append('transferência  {} para conta {} '.format(valor, destino.numero))
 
         else :
             print(f'Procure um Agiota!!!!{self.saldo}')
--- Caixa Eletrônico/main.py
+++ Caixa Eletrônico/main.py
@@ -2,14 +2,16 @@ from conta import*
 from cliente import*
 
 if __name__ == '__main__':
-    cliente = Cliente('Fabrici', 'Dias', 12507830401)
-    conta1 = Conta('123-5', cliente,120.00, 500.00)
-    conta2 = Conta('234-5', 'Debora Firmiano', 200.00, 500.00)
+    cliente = Cliente('Fabrici', 'Dias', '125-078-304-01')
+    conta1 = Conta('123-5', cliente, 120.00, 500.00)
+    cliente2 = Cliente('Brenno' , 'Viana' ,'111-111-000-00')
+    conta2 = Conta('234-5', cliente2, 200.00, 500.00)
 
-    print(type(conta1.numero))
-    print(type(conta1.titular))
-    print(type(conta1.limite))
-
-
-
+    conta1.deposita(50)
+    conta2.transfere_para(conta1,50)
+    conta1.extrato()
+    conta2.extrato()
+    conta1.saca(50)
+    conta1.saca(10)
+    conta1.historico.imprime()
 
